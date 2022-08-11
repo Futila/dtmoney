9ecf1cf12e5ab06cca96232956345c2e2df7df70
Fernando Futila <fernando.futila@gmail.com>
2022-08-11 00:46:23 -0300

    Adicionadas fonts do Google Fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -34,12 +34,22 @@ export const GlobalStyle = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
   }
 
+  body, input, button, textarea {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
   button {
     cursor: pointer;
   }
+
   [disabled] {
     opacity: 0.6;
     cursor: not-allowed;
+  }
+
+  h1,h2,h3,h4,h5,h6,strong {
+    font-weight: 600;
   }
     
 `;
