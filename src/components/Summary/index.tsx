2ef152f3b2ef01b29ddaeda2a5b555f39c41eab5
Fernando Futila <fernando.futila@gmail.com>
2022-08-11 11:50:55 -0300

    criado component Summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,35 @@
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import totalImg from "../../assets/total.svg";
+
+import { Container } from "./styles";
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <span>Entradas</span>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+
+        <strong>R$1000</strong>
+      </div>
+      <div>
+        <header>
+          <span>Saídas</span>
+          <img src={outcomeImg} alt="Saídas" />
+        </header>
+
+        <strong>-R$500</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <span>Total</span>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>R$500</strong>
+      </div>
+    </Container>
+  );
+}
