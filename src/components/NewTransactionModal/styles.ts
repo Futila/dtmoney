4183932b32a11719fc78f2556b918eb61cf3d76d
Fernando Futila <fernando.futila@gmail.com>
2022-08-12 00:32:39 -0300

    Criando botões de tipo

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { darken } from "polished";
 
 export const Container = styled.form`
   h2 {
@@ -15,6 +16,7 @@ export const Container = styled.form`
     background: #e7e9ee;
     font-weight: 400;
     font-size: 1rem;
+
     &::placeholder {
       color: var(--text-body);
     }
@@ -36,6 +38,45 @@ export const Container = styled.form`
     transition: filter 0.2s;
     &:hover {
       filter: brightness(0.9);
+    }
+  }
+`;
+
+export const TransactionTypeContainer = styled.div`
+  width: 100%;
+  margin: 1rem 0;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
+
+  button {
+    height: 4rem;
+    border: 1px solid #d7d7d7;
+    border-radius: 0.25rem;
+
+    background: transparent;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    transition: border-color 0.2s;
+
+    &:hover {
+      border-color: ${darken(0.1, "#d7d7d7")};
+    }
+
+    img {
+      width: 20px;
+      height: 20px;
+    }
+
+    span {
+      display: inline-block;
+      margin-left: 1rem;
+      font-size: 1rem;
+      color: var(--text-title);
     }
   }
 `;
